b4fcea9d0c7dd7e946cf5f44ded2c9af204eca16
anushaalluri01 <154517532+anushaalluri01@users.noreply.github.com>
2025-05-13 15:30:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,34 @@
+# Variable for S3 bucket name
 variable "bucket_name" {
   description = "The name of the S3 bucket (must be globally unique)"
   type        = string
   default     = "my-simple-terraform-bucket-12345" # You can override this with -var option
 }
 
+# Variable for AWS region
 variable "region" {
   description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+# Variable for AWS Access Key ID
+variable "AWS_ACCESS_KEY_ID" {
+  description = "The AWS access key ID"
+  type        = string
+  sensitive   = true
+}
+
+# Variable for AWS Secret Access Key
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS secret access key"
+  type        = string
+  sensitive   = true
+}
+
+# Variable for AWS Region (already defined above)
+variable "AWS_DEFAULT_REGION" {
+  description = "The default AWS region"
   type        = string
   default     = "us-east-1"
 }
