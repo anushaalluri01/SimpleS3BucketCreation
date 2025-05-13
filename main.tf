8503db51f8374bf88b2acb554360f58d29e97bca
Anugrah <anushaalluri28101995@gmail.com>
2025-05-13 17:13:57 +0530

    Updated Terraform configuration and CI/CD pipeline

--- main.tf
+++ main.tf
@@ -5,12 +5,6 @@ terraform {
       version = ">= 4.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
 }
 
 provider "aws" {
