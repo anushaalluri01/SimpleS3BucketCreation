6424892f3d6814f2f77c323c166f2688e93021c2
anushaalluri01 <154517532+anushaalluri01@users.noreply.github.com>
2025-05-13 15:37:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
@@ -22,4 +22,3 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Dev"
   }
 }
-
